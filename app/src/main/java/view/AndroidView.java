5e5411142f6cb25d05fd5597da93a1fb55b54523
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2020-02-14 16:42:13 +0100

    addede Model classen i package Model

--- app/src/main/java/view/AndroidView.java
+++ app/src/main/java/view/AndroidView.java
@@ -1,10 +1,9 @@
-package View;
+package view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
